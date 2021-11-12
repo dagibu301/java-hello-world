2f0f50ef39721064dd8d56bb25eafffa36abb01c
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-12 17:49:15 -0500

    casting

---
+++ src/Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main(String[] args) {
+        double monthlyDogs = 30.0 / 12.0;
+        System.out.println(monthlyDogs);
+
+        int estimatedMonthlyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+    }
+}
