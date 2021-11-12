894bb1959e6fa529daeea7e93ded832d78c8b5ec
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-12 14:17:45 -0500

    variables

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declare variable
+        int speed;
+        // Assign value
+        speed = 10;
+
+        int salary = 10000;
+        String employeeName = "Clark Kent";
+        System.out.println(speed);
+        System.out.println(employeeName + " earns " + salary);
+    }
+}
