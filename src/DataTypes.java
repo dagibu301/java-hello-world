f74c0a4e27335f6c18ce0274409ac062438c6e5b
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-12 14:46:36 -0500

    numbers

---
+++ src/DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte b = 123;
+        short s = 32767;
+        int n = 1234567890;
+        long nL = 1234567890123456789L;
+        float f = 234234.234F;
+        double nD = 123.342;
+    }
+}
