0da1c850cbb94043b6d11cd6bf1491b077d5eb5d
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-12 14:51:41 -0500

    types

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -7,5 +7,16 @@ public class DataTypes {
         long nL = 1234567890123456789L;
         float f = 234234.234F;
         double nD = 123.342;
+
+        char c = 'Y';
+
+        boolean isTrue = true;
+
+        var salary = 10000;
+        var pension = salary * 0.03;
+        
+        var totalSalary = salary - pension;
+
+        System.out.println(totalSalary);
     }
 }
