21e7871be13986fc7680068d0431c622a96db0a1
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-13 13:18:07 -0500

    if

---
+++ src/IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = true;
+
+        if (isBluetoothEnabled) {
+            System.out.println("File has been sent");
+        } else {
+            System.out.println("Error, no bluetooth");
+        }
+    }
+}
