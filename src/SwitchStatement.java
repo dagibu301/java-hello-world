bf2750fe1c49964bd6df05dd15971d082069b02d
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-13 15:03:54 -0500

    docs

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -2,7 +2,16 @@ public class SwitchStatement {
     public static void main(String[] args) {
 
         int age = 20;
+        printAge(age);
+    }
 
+    /**
+     * Description: Switch to print age
+     *
+     * @param age age of the person
+     * @return void
+     * */
+    public static void printAge(int age) {
         switch (age) {
             case 1 -> System.out.println("You are 1 year old!");
             case 20 -> System.out.println("You are 20 years old!");
