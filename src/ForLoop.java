6aeb5881c363bfb10305c96dceaafc94d8e5b41c
David Giraldo <davidgiraldobueno@gmail.com>
2021-11-13 15:23:04 -0500

    for

---
+++ src/ForLoop.java
@@ -0,0 +1,15 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for(int i = 5; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
